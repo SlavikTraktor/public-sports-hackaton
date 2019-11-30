ec0c46c0173afadcde5b32698eb07a4221cd44bb
Slava Traktor <slavasobb@gmail.com>
2019-11-30 23:25:30 +0300

    URLS fix

--- client/src/common/components/Header.jsx
+++ client/src/common/components/Header.jsx
@@ -1,30 +1,30 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { Icon } from '../utils/Icon';
-import * as Consts from '../constants/constants';
+import { URLS } from '../constants/constants';
 
 export const Header = () => (
   <header>
     <div className="wrapper">
       <nav>
         <div className="main-nav">
-          <Link to={Consts.$NotificatinosLink} className="nav-item active">
+          <Link to={URLS.NOTIFICATION_PAGE} className="nav-item active">
             Уведомления
           </Link>
-          <Link to={Consts.$SpotsLink} className="nav-item active">
+          <Link to={URLS.SPOTS_PAGE} className="nav-item active">
             Подписки
           </Link>
-          <Link to={Consts.$UserProfileLink} className="nav-item active">
+          <Link to={URLS.PROFILE_PAGE} className="nav-item active">
             Профиль
           </Link>
         </div>
         <div className="main-nav">
-          <Link to={Consts.$MapPageLink} className="nav-icon">
+          <Link to={URLS.MAP_PAGE} className="nav-icon">
             <Icon name="map" width={40} height={40} />
           </Link>
           <div className="nav-separator" />
-          <Link to={Consts.$SearchLink} className="nav-icon">
-             <Icon name="search" width={40} height={40} />
+          <Link to={URLS.SERCH_PAGE} className="nav-icon">
+            <Icon name="search" width={40} height={40} />
           </Link>
         </div>
       </nav>
