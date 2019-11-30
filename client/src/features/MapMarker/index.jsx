52ddcbf51bf2050e5a4d1df8e5fe6daec8fb87db
Slava Traktor <slavasobb@gmail.com>
2019-11-30 16:20:02 +0300

    MapMarker with popup

---
+++ client/src/features/MapMarker/index.jsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Marker, Popup } from 'react-leaflet';
+
+const mockData = {
+  title: 'Площадка *name*',
+  id: 'id#0001',
+  adress: 'школа №525',
+  paramsList: ['На площадке 20 человек', 'Бесплатно', 'Есть освещение', 'Открыто круглосуточно'],
+  sportTypes: ['workout', 'football', 'basketball', 'volleyball'],
+};
+
+export const MapMarker = ({ position }) => {
+  return (
+    <Marker position={position}>
+      <Popup className="custom-map-marker">
+        <div className="popup-title">
+          <div className="title-top">
+            <h3>{mockData.title}</h3>
+            <div className="id">{mockData.id}</div>
+          </div>
+          <div className="title-bot">{mockData.adress}</div>
+        </div>
+        <div className="popup-body">
+          {mockData.paramsList.map((v, i) => (
+            <div key={i} className="popup-body-param">{v}</div>
+          ))}
+        </div>
+        <div className="popup-footer">
+          {mockData.paramsList.map((v, i) => (
+            <div key={i} className="popup-footer-icon">O</div>
+          ))}
+        </div>
+      </Popup>
+    </Marker>
+  );
+};
