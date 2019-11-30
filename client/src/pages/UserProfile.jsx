b6cb108b5897e0224d7c080c8fcb5a7cf56a80c8
Narek Avagyan <ava.narek@gmail.com>
2019-12-01 01:29:00 +0300

    profile refactoring

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,69 +1,74 @@
 import * as React from 'react';
+import { SportIcons } from '../common/utils/SportIcons';
+import { Icon } from '../common/utils/Icon';
 
 export const UserProfile = () => (
-  <div className="wrapper">
-    <div className="userprofile-body">
-      <div className="userprofile-topblock">
-        <div className="userprofile-leftblock">
-          <div className="userprofile-avatar" />
-          <button type="button" className="userprofile-graybutton">
-            <div className="text">Сменить аватар</div>
-          </button>
-          <button type="button" className="userprofile-graybutton">
-            <div className="text">Редактировать профиль</div>
-          </button>
-        </div>
-        <div className="userprofile-rightblock">
-          <div className="rightblock-Name">Имя Фамилия</div>
-          <div className="rightblock-playerid">id #000001</div>
-          <div className="rightblock-playerdata">
-            <div className="playerdata-leftpart">
-              <div className="leftpart-item">
-                День
-                <br />
-                Рождения:
-              </div>
-              <div className="leftpart-item">Город:</div>
-              <div className="leftpart-item">Интересы:</div>
-            </div>
-            <div className="playerdata-rightpart">
-              <div className="rightpart-item">01 января 1970</div>
-              <div className="rightpart-item">Санкт-Петербург</div>
-              <div className="rightpart-item">
-                <div className="icons">
-                  <div className="football-icon" />
-                  <div className="basketball-icon" />
-                </div>
-              </div>
-            </div>
+  <div className="userprofile-body">
+    <div className="userprofile-topblock">
+      <div className="userprofile-leftblock">
+        <div className="userprofile-avatar" />
+      </div>
+      <div className="userprofile-rightblock">
+        <div className="rightblock-Name">Имя Фамилия <span><a href="vk.com/#id">vk.com/#id</a></span></div>
+        <div className="rightblock-playerid">id #000001</div>
+        <div className="user-info">
+          <div className="info-item">
+            День рождения:<span>01 января 1970</span>
           </div>
-          <div className="rightblock-playerrating">
-            <div className="playerrating-title">Рейтинг игрока</div>
+          <div className="info-item">
+            Город:<span>Санкт-Петербург</span>
           </div>
-          <div className="playerrating-blocks">
-            <div className="block">
-              <div className="dark-block" />
-              <div className="football-icon" />
-              <div className="text">544</div>
-            </div>
-            <div className="block">
-              <div className="dark-block" />
-              <div className="basketball-icon" />
-              <div className="text">321</div>
-            </div>
+          <div className="info-item">
+            Интересы:
+            <span>
+              <div className="icons">
+                <div className="football-icon" />
+                <div className="basketball-icon" />
+              </div>
+            </span>
           </div>
         </div>
       </div>
-      <div className="userprofile-buttonsblock">
-        <button type="button" className="button">
-          <i className="icon" />
-          Мои места
+    </div>
+    <div className="rating-row">
+      <div className="edit-buttons">
+        <button type="button" className="userprofile-graybutton">
+          <div className="text">Сменить аватар</div>
         </button>
-        <button type="button" className="button">
-          <i className="icon2" />
-          Мои друзья
+        <button type="button" className="userprofile-graybutton">
+          <div className="text">Редактировать профиль</div>
         </button>
       </div>
+      <div className="rating">
+        <div className="rightblock-playerrating">Рейтинг игрока</div>
+        <div className="playerrating-blocks">
+          <div className="block">
+            <div className="dark-block" />
+            <div className="rating-icon">
+              <SportIcons sports={['football']} color="#414b50" />
+            </div>
+            <div className="text">550</div>
+          </div>
+          <div className="block">
+            <div className="dark-block" />
+            <div className="rating-icon">
+              <SportIcons sports={['basketball']} color="#414b50" />
+            </div>
+            <div className="text">550</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="userprofile-buttonsblock">
+      <button type="button" className="button">
+        <Icon name="pointer" color="#414b50" />
+        <span>мои места</span>
+      </button>
+      <button type="button" className="button">
+        <Icon name="users" color="#414b50" />
+        <span>мои друзья</span>
+      </button>
     </div>
   </div>
 );
