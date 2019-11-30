9f953f127d0f7445a0c10f8a5f3a1dabb4472dcc
Slava Traktor <slavasobb@gmail.com>
2019-11-30 11:41:11 +0300

    Init

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# public-sports-hackaton
+# Public-sports
+This is a social network about sport spots.
+
+# Licence
+This project is distributed under MIT [license](./LICENSE).
+
+# Frontend
+[Frontend organization](https://blogru.4xxi.com/%D0%BE%D1%80%D0%B3%D0%B0%D0%BD%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F-%D0%BA%D0%BE%D0%B4%D0%B0-%D0%B2-%D0%BC%D0%B0%D1%81%D1%88%D1%82%D0%B0%D0%B1%D0%BD%D1%8B%D1%85-react-%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B0%D1%85-bc00ce1621e3)
+
+# Demo
+To start you should go to client folder and type `npm i` command, then `npm start`.
+
+# Development
+Run `git config core.hooksPath .githooks` in project folder before you'll make first commit. This command enables githook that runs eslint before every commit.
---
+++ client/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  devServer: {
+    index: "index.html",
+    contentBase: ["public", "dist"],
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
+    },
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
