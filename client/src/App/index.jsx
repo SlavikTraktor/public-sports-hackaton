eb97f08e1d02407a1743bb10df604767ddb83b68
Narek Avagyan <ava.narek@gmail.com>
2019-11-30 15:49:18 +0300

    router

--- client/src/App/index.jsx
+++ client/src/App/index.jsx
@@ -1,3 +1,22 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navigation } from '../common/components/Navigation';
+import { Notifications } from '../pages/Notifications';
 
-export const App = () => <div className="wrapper">keklul</div>;
+const Spots = () => <div/>;
+const Search = () => <div/>;
+const MapPage = () => <div/>;
+const Profile = () => <div/>;
+
+export const App = () => (
+  <Router>
+    <Navigation />
+    <Switch>
+      <Route path="/notifications" component={Notifications} />
+      <Route path="/spots" component={Spots} />
+      <Route path="/search" component={Search} />
+      <Route path="/map" component={MapPage} />
+      <Route path="/profile" component={Profile} />
+    </Switch>
+  </Router>
+);
--- client/src/pages/Notifications.jsx
+++ client/src/pages/Notifications.jsx
@@ -3,23 +3,20 @@ import { Navigation } from '../common/components/Navigation';
 import { Notification } from '../common/components/Notification';
 
 export const Notifications = () => (
-  <>
-    <Navigation />
-    <div className="notification-page">
-      <div className="wrapper">
-        <aside className="select-type">
-          <div className="type-item active">Площадки</div>
-          <div className="type-item">Друзья</div>
-          <div className="type-item">Другое</div>
-        </aside>
-        <div className="notification-list">
-          <Notification
-            name="author"
-            text="Площадка закрыта на ремонт"
-            date="2019-11-30T13:17:23+03:00"
-          />
-        </div>
+  <div className="notification-page">
+    <div className="wrapper">
+      <aside className="select-type">
+        <div className="type-item active">Площадки</div>
+        <div className="type-item">Друзья</div>
+        <div className="type-item">Другое</div>
+      </aside>
+      <div className="notification-list">
+        <Notification
+          name="author"
+          text="Площадка закрыта на ремонт"
+          date="2019-11-30T13:17:23+03:00"
+        />
       </div>
     </div>
-  </>
+  </div>
 );
