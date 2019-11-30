82b514fe2b0247610e6db6bc71de2b1c84329ae5
Shad <sdwstorm@gmail.com>
2019-11-30 20:38:23 +0300

    Profile-page

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,14 @@
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import * as React from 'react';
-import { Notification } from './pages/Notification';
+import { UserProfile } from './pages/UserProfile';
 import { store } from './store';
 
 import './scss/index.scss';
 
 render(
   <Provider store={store}>
-    <Notification />
+    <UserProfile />
   </Provider>,
   document.getElementById('root'),
 );
