d4146e5ddb7cbb43ac98cb90701341429eaa6fe7
Narek Avagyan <ava.narek@gmail.com>
2019-12-01 04:33:00 +0300

    profile bug fix

--- client/src/common/components/CommentArea.jsx
+++ client/src/common/components/CommentArea.jsx
@@ -2,30 +2,34 @@ import * as React from 'react';
 import moment from 'moment';
 import { Icon } from '../utils/Icon';
 
-export const CommentArea = (props) => {
-  const renderComments = () => props.map(({
-    name, date, text, likes,
-  }) => (
-    <div className="comment">
-      <div className="comment-content">
-        <div className="comment-header">
-          <h3 className="title">
-            {name}
-            <div>{date}</div>
-          </h3>
+export const CommentArea = props => {
+  const renderComments = () => {
+    if (props.comments) {
+      return props.comments.map(({ name, date, text, likes }) => (
+        <div className="comment">
+          <div className="comment-content">
+            <div className="comment-header">
+              <h3 className="title">
+                {name}
+                <div>{date}</div>
+              </h3>
+            </div>
+            <div className="comment-body">{text}</div>
+            <div className="separator" />
+            <div className="likes">
+              <div className="heart-icon" />
+              <span>{likes}</span>
+            </div>
+          </div>
+          <div className="trash-icon">
+            <Icon name="trashbox" />
+          </div>
         </div>
-        <div className="comment-body">{text}</div>
-        <div className="separator" />
-        <div className="likes">
-          <div className="heart-icon" />
-          <span>{likes}</span>
-        </div>
-      </div>
-      <div className="trash-icon">
-        <Icon name="trashbox" />
-      </div>
-    </div>
-  ));
+      ));
+    } else {
+      return <div class="placeholder">У Вас пока нет записей <span>&#128532;</span> <br/>Создайте свою первую запись <span>&#128523;</span></div>;
+    }
+  };
 
   return (
     <>
